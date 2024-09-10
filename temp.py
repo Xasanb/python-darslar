aab40da6f6682061dbcace20b19bf1902fa86261
Xasanboy <xsobirov909@gmail.com>
2024-09-10 21:58:03 +0500

    codni shunchaki qoymi
    xxx

---
+++ temp.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Редактор Spyder
+
+Это временный скриптовый файл.
+"""
+
+'''1
+menu = ['osh','kabob','shorva','jaz']
+buy = input("nima ovqat yeysiz? ")
+if buy.lower() in menu:
+     print("Buyurtma qabul qilindi!")
+else: 
+    print("bunday ovqat bizda yoq")
+    
+'''
+
+cars = ["mers","byd","bmw","tesla","hyundai"]
+
+
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+        
+    if car == 'byd':
+        print(car.upper())
+    else:
+        print(car.title())
